983317276fed893e85463c1cc6dab757d52927a7
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-12 14:27:24 -0300

    Create Ex_02.py

---
+++ EstruturaSequencial/Ex_02.py
@@ -0,0 +1,7 @@
+"""
+2.Faça um Programa que peça um número e então mostre a mensagem O número 
+informado foi [número].
+"""
+
+numero = input('Digite um número: ') #Tipo string
+print(f'O número informado foi {numero}')
