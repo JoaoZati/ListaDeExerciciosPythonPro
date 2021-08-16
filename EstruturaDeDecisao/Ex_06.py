55a46fed6721a9dd344764d800e7c03fc89cd6e5
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-16 16:04:51 -0300

    Create Ex_06.py

---
+++ EstruturaDeDecisao/Ex_06.py
@@ -0,0 +1,7 @@
+#%% 06 - Faça um Programa que leia três números e mostre o maior deles.
+
+numero_1 = float(input('Insira um Numero: '))
+numero_2 = float(input('Insira outro número: '))
+numero_3 = float(input('Insira outro número: '))
+
+print(max(numero_1, numero_2, numero_3))
