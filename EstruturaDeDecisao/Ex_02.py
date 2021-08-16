73f5571ea43c22255209d386e2ee672af872fb42
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-16 16:03:51 -0300

    Create Ex_02.py

---
+++ EstruturaDeDecisao/Ex_02.py
@@ -0,0 +1,8 @@
+#%% 02 - Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+numero_1 = float(input('Insira um Numero: '))
+
+if numero_1 >= 0:
+    print(f'O numero é positivo: {numero_1}')
+else:
+    print(f' O numero é negativo: {numero_1}')
