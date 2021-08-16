0c1902330c7ffbf207bfc06e6582601c362956c2
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-16 17:56:43 -0300

    Create Ex_09.py

---
+++ EstruturaDeRepeticao/Ex_09.py
@@ -0,0 +1,11 @@
+#%% 09 - Numeros impares 1 a 50
+
+"""
+Faça um programa que imprima na tela apenas os números ímpares entre 
+1 e 50.
+"""
+
+numero = 50
+
+for i in range(1, numero+1, 2):
+    print(i)
