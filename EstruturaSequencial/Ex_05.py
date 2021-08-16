dd0af4ec2dba1abbebfbd3fe0085e5593d8030ef
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-16 15:55:02 -0300

    Create Ex_05.py

---
+++ EstruturaSequencial/Ex_05.py
@@ -0,0 +1,6 @@
+#%% 05 - Faça um Programa que converta metros para centímetros.
+
+metro_imp = float(input('Digite um valor em metros para converter em cm: '))
+centrimetro_imp = metro_imp*100
+
+print(f'O valor em cm é igual a: {centrimetro_imp}cm')
