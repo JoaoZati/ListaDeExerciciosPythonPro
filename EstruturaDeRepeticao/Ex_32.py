d6166e277fc7678aaa3ba9b50de25edbe28d220e
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-16 18:08:59 -0300

    Create Ex_32.py

---
+++ EstruturaDeRepeticao/Ex_32.py
@@ -0,0 +1,29 @@
+#%% 32 - Fatorial novamente
+
+"""
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário. Ex.: 5!=5.4.3.2.1=120. A saída deve ser conforme o exemplo abaixo:
+Fatorial de: 5
+5! =  5 . 4 . 3 . 2 . 1 = 120
+"""
+
+while True:
+    try:
+        num_f = int(input('Insira um numero inteiro: '))
+    except ValueError:
+        print('Não foi fornecido um numero inteiro')
+    else:
+        break
+
+string = f'{num_f}!='
+fatorial = 1
+
+for i in range(num_f, 0, -1):
+    string += f'{i}'
+    fatorial *= i
+    if i == 1:
+        string += '='
+    else:
+        string += '.'
+
+string += f'{fatorial}'
+print(f'{string}')  
