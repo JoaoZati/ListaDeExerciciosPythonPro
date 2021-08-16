eaffd7a292396b66fb71c780bc7d23e2beffe825
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-16 16:16:57 -0300

    Update Ex_02.py

--- EstruturaDeRepeticao/Ex_02.py
+++ EstruturaDeRepeticao/Ex_02.py
@@ -1,5 +1,3 @@
-#%% 02 - Nome e senha
-
 """
 Faça um programa que leia um nome de usuário e a sua senha e não aceite a 
 senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando
@@ -13,7 +11,6 @@ while True:
         
     if nome_usuario != senha_usuario:
         print('Nome e senha válidos')
-        continue
-    break
+        break
     
     print('Sorry, o nome e a senha não podem ser a mesma: ')
