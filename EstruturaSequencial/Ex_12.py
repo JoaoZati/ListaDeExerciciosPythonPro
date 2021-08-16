a8798ca8526fcdbe91d128e36897980961a02abb
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-16 15:57:30 -0300

    Create Ex_12.py

---
+++ EstruturaSequencial/Ex_12.py
@@ -0,0 +1,12 @@
+#%% 12 - Calculo Altura
+"""
+Tendo como dados de entrada a altura de uma pessoa, 
+construa um algoritmo que calcule seu peso ideal, 
+usando a seguinte fórmula: 
+(72.7*altura) - 58
+"""
+
+altura = float(input('Digite a altura em metros: '))
+peso_ideal = (72.7*altura) - 58
+
+print(f'O peso ideial é de: {peso_ideal}')
