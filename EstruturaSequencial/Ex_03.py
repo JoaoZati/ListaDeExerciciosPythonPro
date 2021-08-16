71c7c13086c782dcbc5ab6bbc92fa4f6d866bb8d
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-16 15:54:35 -0300

    Create Ex_03.py

---
+++ EstruturaSequencial/Ex_03.py
@@ -0,0 +1,9 @@
+#%% 03 - Faça um Programa que peça dois números e imprima a soma.
+print('Soma de Dois Numeros')
+
+numero_1 = int(input('Digite primeiro número: '))
+numero_2 = int(input('Digite Segundo número: '))  #Operação de soma
+#print(type(numero_1))
+
+soma = numero_1 + numero_2
+print(f'A soma dos dois números é: {soma}')
