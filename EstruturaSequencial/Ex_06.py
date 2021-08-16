80362b33741dd449d78a376a8722f155028f2da4
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-16 15:55:25 -0300

    Create Ex_06.py

---
+++ EstruturaSequencial/Ex_06.py
@@ -0,0 +1,6 @@
+#%% 06 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio_cir = float(input('Digite um valor em metros do raio do circulo para calcular a área: '))
+area_cir =  (raio_cir**2)*3.1415
+
+print(f'O valor da área do circulo de raio {raio_cir} é igual a: {area_cir}')
