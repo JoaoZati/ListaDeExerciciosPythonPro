d2d723991ccede9c3c715558f615a1e26f880970
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-16 15:56:41 -0300

    Create Ex_10.py

---
+++ EstruturaSequencial/Ex_10.py
@@ -0,0 +1,6 @@
+#%% 10 - Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
+temp_c =  float(input('Digite um valor da temperatura Celsius: '))
+Fahrenheit = ((temp_c/5)*9)+32
+
+print(f'A temperatura em Fahrenheit é igual a: {Fahrenheit} Cº')
