4eb89522100514f1bd51cef2383e9a1c01fcec7e
JoaoZati <35962516+JoaoZati@users.noreply.github.com>
2021-08-17 18:48:16 -0300

    Create Ex_03.py

---
+++ ExerciciosComString/Ex_03.py
@@ -0,0 +1,10 @@
+#%% 03 - Nome na vertical
+
+"""
+Nome na vertical. Faça um programa que solicite o nome do usuário e 
+imprima-o na vertical.
+"""
+
+string = input('Digite uma palavra: ')
+
+[print(i) for i in string]
